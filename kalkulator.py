0f45ae8fb5b1697a823d45b58f4db726148c4056
Asido Saputra <asidosaputra@Asidos-MacBook-Air.local>
2025-09-22 09:38:09 +0700

    12115023_Asido Saputra Sigalingging

---
+++ kalkulator.py
@@ -0,0 +1,10 @@
+import matematika
+from matematika import perkalian
+
+a = 5
+b = 6
+
+hasil_jumlah = matematika.pengurangan(a, b)
+print(hasil_jumlah)
+# hasil_kali = perkalian(a, b)
+# print(hasil_kali)
